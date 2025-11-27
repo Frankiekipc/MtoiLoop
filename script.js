d8a23d8a84dbc9b6433915f113df33458d97c5d5
Frankiekipc <frankrkipchumba@gmail.com>
2025-11-27 14:15:14 +0300

    Day 1 –landing plain

---
+++ script.js
@@ -0,0 +1,6 @@
+function selectRole(role) {
+    const result = document.getElementById("result");
+    result.textContent = role + "selected for MtoiLoop";
+    console.log("User role:", role);
+
+}
