53d29293025b20da51fe81c471f6998bae25ae93
Frankiekipc <frankrkipchumba@gmail.com>
2025-12-19 13:18:30 +0300

    frontend of mtoi loop

--- script.js
+++
@@ -1,6 +0,0 @@
-function selectRole(role) {
-    const result = document.getElementById("result");
-    result.textContent = role + "selected for MtoiLoop";
-    console.log("User role:", role);
-
-}
